501030b7bb6205d30b1021a7d34ccb7ddfc6b605
Sapna1982 <sapnasharma700@hotmail.com>
2023-02-27 19:32:34 +0000

    2nd commit

---
+++ SeleniumHomework/LoginFirefox.java
@@ -0,0 +1,25 @@
+package SeleniumHomework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoginFirefox {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+
+        Thread.sleep(4000);
+
+        driver.findElement(By.xpath("/html/body/div[6]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
+        driver.findElement(By.id("Email")).sendKeys("sapnasharma700@hotmail.com");
+        driver.findElement(By.id("Password")).sendKeys("123456");
+        driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/div[1]/div[2]/form/div[3]/button")).click();
+
+    }
+}
